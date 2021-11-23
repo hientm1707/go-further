70fb6af59a7492213897a5b92e2dc16b9f94a914
hientm177 <minhhien1772000@gmail.com>
2021-11-23 11:04:42 +0700

    init project

---
+++ src/main/java/vn/com/momo/baseproject/BaseprojectApplication.java
@@ -0,0 +1,15 @@
+package vn.com.momo.baseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseprojectApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
+q
