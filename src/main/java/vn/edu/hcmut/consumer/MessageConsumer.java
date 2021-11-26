9c430657e65856ae2ec1cf866798a524e079045e
hientm177 <minhhien1772000@gmail.com>
2021-11-26 11:50:14 +0700

    temp

--- src/main/java/vn/edu/hcmut/consumer/MessageConsumer.java
+++ src/main/java/vn/edu/hcmut/consumer/MessageConsumer.java
@@ -1,21 +1,21 @@
 package vn.edu.hcmut.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import vn.edu.hcmut.constant.QueueConstant;
-import vn.edu.hcmut.dto.MessageDTO;
 
 @Component
 public class MessageConsumer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
     @RabbitListener(queues = QueueConstant.QUEUE1)
-    public void consumeMessageFromQueue(Message messageDTO) {
+    public void consumeMessageFromQueue(Message messageDTO) throws InterruptedException {
         System.out.println("======Received a message from gateway, sending ACK====");
+        Thread.sleep(3000);
         rabbitTemplate.convertAndSend(QueueConstant.EXCHANGE_NAME, QueueConstant.ROUTING_KEY2, messageDTO.toString());
     }
 }
